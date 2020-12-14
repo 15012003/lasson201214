6210d1c18ab63f4b6619abe3013e5ceeedcaea8a
Student <vasnas2003@gmail.com>
2020-12-14 12:07:16 +0500

    1

---
+++ stap_1.py
@@ -0,0 +1,11 @@
+thisdict = {
+  "brand": "Mersedes",
+  "model": "S63",
+  "year": 2013,
+  "year": 2020,
+"colors": ["red", "white", "silver"]
+}
+# print(thisdict)
+# print(thisdict["brand"])
+# print(thisdict)
+print(thisdict["colors"] [2])
